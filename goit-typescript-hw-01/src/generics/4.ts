752e42606291bc41e49a2a3c7c63b8ea9371e599
Anna <kupriienko16@gmail.com>
2024-05-27 12:17:41 +0100

    commit

--- goit-typescript-hw-01/src/generics/4.ts
+++ goit-typescript-hw-01/src/generics/4.ts
@@ -1,15 +1,22 @@
 type User = {
-    name: string;
-    surname: string;
-    email: string;
-    password: string;
+name: string;
+surname: string;
+email: string;
+password: string;
 }
 
-function createOrUpdateUser(initialValues: Partial<User>) {
-    
+function createOrUpdateUser(initialValues: Partial<User>): User {
+  const currentUser: User = {
+    name: 'Anna',
+    surname: 'Kamil',
+    email: 'user1@mail.com',
+    password: 'password12334',
+  };
+  const updatedUser = { ...currentUser, ...initialValues };
+  return updatedUser
 }
 
 createOrUpdateUser({ 
   email: 'user@mail.com', 
-  password: 'password123' 
+  password: 'password123'
 });
